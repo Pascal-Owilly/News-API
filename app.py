79ac779284266d90a5ffdfbdb02d18b3d7bc20b1
Owilly <pasaclouma54@gmail.com>
2022-02-26 15:18:10 +0300

    fetch news from BBC and sispaly on the home page

--- app.py
+++ app.py
@@ -2,12 +2,46 @@
 # import Flask from flask module
 # Then initialize our app instance using the imported Flask
 from distutils.log import debug
+from importlib.resources import contents
 from flask import Flask,render_template
+from newsapi import NewsApiClient
 
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return render_template("index.html")
+    # To get athorize we use the id and the api_key
+    # We use sources provided by the news sites
+    newsapi = NewsApiClient(api_key="88cdeb5bea694d6888cd2dda6e6fae00")
+    top_news = newsapi.get_top_headlines(sources = 'bbc-news') 
+     
+    # Fetching articles of the top newss headlines
+    top_articles = top_news['articles']
+
+    # Creating empty lists to store the values
+    news = []
+    description = []
+    images = []
+    date_published = []
+    news_url = []
+
+# Use for loops to store all our contents
+    for i in range(len(top_articles)):
+        top_news_articles = top_articles[i]
+
+        # apppend articles into the lists
+        news.append(top_news_articles['title'])
+        description.append(top_news_articles['description'])
+        images.append(top_news_articles['urlToImage'])
+        date_published.append(top_news_articles['publishedAt'])
+        news_url.append(top_news_articles['url'])
+
+        # Make a zip to find the contents directly
+    articles = zip(news,description,images,date_published,news_url)
+# We now render this in our .html
+    return render_template("index.html",articles=articles)  
+
+ 
+ 
 
 if __name__=='__main__':
     app.run(debug=True)
